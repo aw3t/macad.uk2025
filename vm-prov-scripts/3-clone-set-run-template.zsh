493a8851b089e85453f31ef3d66456c54a7d45a9
Aaron Wood <aw3t@virginia.edu>
2025-12-18 11:54:09 -0500

    incrementing clone name

--- vm-prov-scripts/3-clone-set-run-template.zsh
+++ vm-prov-scripts/3-clone-set-run-template.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-tart clone DevJamf-TahoeVM DevJamf-TahoeVM2
-tart set DevJamf-TahoeVM2 --display-refit --random-serial --random-mac
-tart run DevJamf-TahoeVM2
+tart clone DevJamf-TahoeVM DevJamf-TahoeVM1
+tart set DevJamf-TahoeVM1 --display-refit --random-serial --random-mac
+tart run DevJamf-TahoeVM1
