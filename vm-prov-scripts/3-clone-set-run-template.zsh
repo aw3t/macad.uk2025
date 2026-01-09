28e352be161b32532233e80cfaeacbb15e69dc76
Aaron Wood <aw3t@virginia.edu>
2026-01-09 13:25:19 -0500

    changed source and dest. names

--- vm-prov-scripts/3-clone-set-run-template.zsh
+++ vm-prov-scripts/3-clone-set-run-template.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-tart clone Prod-TahoeTemplate Prod-TahoeVM1
-tart set Prod-TahoeVM1 --display-refit --random-serial --random-mac
-tart run Prod-TahoeVM1
+tart clone Dev-TahoeDefender Dev-TahoeDefender1
+tart set Dev-TahoeDefender1 --display-refit --random-serial --random-mac
+tart run Dev-TahoeDefender1
