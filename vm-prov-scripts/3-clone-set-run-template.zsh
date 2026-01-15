7bed75daae495b5e25097ac591c99c84a8a73045
Aaron Wood <aw3t@virginia.edu>
2026-01-15 09:23:52 -0500

    edits

--- vm-prov-scripts/3-clone-set-run-template.zsh
+++ vm-prov-scripts/3-clone-set-run-template.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-tart clone Dev-TahoeDefender Dev-TahoeDefender1
-tart set Dev-TahoeDefender1 --display-refit --random-serial --random-mac
-tart run Dev-TahoeDefender1
+tart clone Dev-TahoeTemplate Dev-TahoeVM1
+tart set Dev-TahoeVM1 --display-refit --random-serial --random-mac
+tart run Dev-TahoeVM1
