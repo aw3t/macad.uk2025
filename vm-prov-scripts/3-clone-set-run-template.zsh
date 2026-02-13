a134999de61b063f96dd6d39029af97b7f1762a7
Aaron Wood <aw3t@virginia.edu>
2026-02-13 08:24:47 -0500

    changed clone script from dev to prod

--- vm-prov-scripts/3-clone-set-run-template.zsh
+++ vm-prov-scripts/3-clone-set-run-template.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-tart clone Dev-TahoeTemplate Dev-TahoeVM1
-tart set Dev-TahoeVM1 --display-refit --random-serial --random-mac
-tart run Dev-TahoeVM1
+tart clone Prod-TahoeTemplate Prod-TahoeVM1
+tart set Prod-TahoeVM1 --display-refit --random-serial --random-mac
+tart run Prod-TahoeVM1
