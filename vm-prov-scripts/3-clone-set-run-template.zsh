24893524bf0cb759a62b48f186c95822b55f7a8f
Aaron Wood <aw3t@virginia.edu>
2026-02-16 14:32:26 -0500

    changed clone file to VM3

--- vm-prov-scripts/3-clone-set-run-template.zsh
+++ vm-prov-scripts/3-clone-set-run-template.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-tart clone Prod-TahoeTemplate Prod-TahoeVM2
-tart set Prod-TahoeVM2 --display-refit --random-serial --random-mac
-tart run Prod-TahoeVM2
+tart clone Prod-TahoeTemplate Prod-TahoeVM3
+tart set Prod-TahoeVM3 --display-refit --random-serial --random-mac
+tart run Prod-TahoeVM3
