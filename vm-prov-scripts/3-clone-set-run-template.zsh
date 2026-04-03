ea2b18b465ff16a19be00e9e3de561f0eb5dc463
Aaron Wood <aw3t@virginia.edu>
2026-04-03 13:38:41 -0400

    changed the validate and clone files to use the 26.4 ipsw.

--- vm-prov-scripts/3-clone-set-run-template.zsh
+++ vm-prov-scripts/3-clone-set-run-template.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-tart clone Dev-TahoeTemplate Dev-TahoeVM1
-tart set Dev-TahoeVM1 --display-refit --random-serial --random-mac
-tart run Dev-TahoeVM1
+tart clone Dev-26_4template Dev-26_4vm1
+tart set Dev-26_4vm1 --display-refit --random-serial --random-mac
+tart run Dev-26_4vm1
